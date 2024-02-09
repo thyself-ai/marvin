53c9bfaa6076a331aa615a5f11fd5a8d5621811f
Lawrence Wang <l@lw.io>
2024-02-09 14:23:27 +0800

    add gpt-4-turbo-preview

--- src/marvin/engine/language_models/openai.py
+++ src/marvin/engine/language_models/openai.py
@@ -18,6 +18,7 @@ CONTEXT_SIZES = {
     "gpt-3.5-turbo-16k": 16384,
     "gpt-3.5-turbo-0613": 4096,
     "gpt-3.5-turbo": 4096,
+    "gpt-4-turbo-preview": 128000,
     "gpt-4-32k-0613": 32768,
     "gpt-4-32k": 32768,
     "gpt-4-0613": 8192,
